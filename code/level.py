c425b5f7a9237f98a4de25644ce60968cf476697
Samir <sami.mir.aner@gmail.com>
2022-09-01 16:05:25 +0500

    create the player

--- code/level.py
+++ code/level.py
@@ -1,5 +1,6 @@
 import pygame
 from tiles import Tile
+from player import Player
 import settings
 
 
@@ -13,15 +14,28 @@ class Level:
 
     def setup_level(self, layout):
         self.tiles = pygame.sprite.Group()
+        self.player = pygame.sprite.GroupSingle()
+
         for row_index, row in enumerate(layout):
             for col_index, cell in enumerate(row):
                 # print(f"{row_index},{col_index}:{cell}")
+                x = col_index * settings.tile_size
+                y = row_index * settings.tile_size
+
                 if cell == 'X':
-                    x = col_index * settings.tile_size
-                    y = row_index * settings.tile_size
                     tile = Tile((x, y), settings.tile_size)
                     self.tiles.add(tile)
+                if cell == 'P':
+                    player_sprite = Player((x, y))
+                    self.player.add(player_sprite)
+
 
     def run(self):
+
+        # level tiles
         self.tiles.update(self.world_shift)
         self.tiles.draw(self.display_surface)
+
+        # player
+        self.player.update()
+        self.player.draw(self.display_surface)
