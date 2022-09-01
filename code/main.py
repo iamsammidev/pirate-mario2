e324f927b87fc82cf524ec975ed3d5101ddb1a6b
Samir <sami.mir.aner@gmail.com>
2022-09-01 15:29:43 +0500

    setting up the level

---
+++ code/main.py
@@ -0,0 +1,23 @@
+import pygame
+import sys
+import settings
+from level import Level
+
+# INI PYGAME
+pygame.init()
+screen = pygame.display.set_mode((settings.screen_width, settings.screen_height))
+clock = pygame.time.Clock()
+level = Level(settings.level_map, screen)
+
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    screen.fill('black')
+    level.run()
+
+    pygame.display.update()
+    clock.tick(settings.fps)
---
+++ code/settings.py
@@ -0,0 +1,28 @@
+level_map = [
+'                            ',
+'                            ',
+'                            ',
+' XX    XXX            XX    ',
+' XX P                       ',
+' XXXX         XX         XX ',
+' XXXX       XX              ',
+' XX    X  XXXX    XX  XX    ',
+'       X  XXXX    XX  XXX   ',
+'    XXXX  XXXXXX  XX  XXXX  ',
+'XXXXXXXX  XXXXXX  XX  XXXX  ']
+
+tile_size = 64
+screen_width = 1200
+screen_height = len(level_map) * tile_size
+fps = 60
+
+
+if __name__ == '__main__':
+    print(len(level_map) * tile_size)
+    # stro = 0
+    # for stroka in level_map:
+    #     for symbol in stroka:
+    #         if symbol == "X":
+    #             print('ПРЫК-СКОК')
+    #         if symbol == " ":
+    #             print("Тыг-Дык")
