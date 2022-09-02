754a580a4bb99975386d151d928060ec774e728a
Samir <sami.mir.aner@gmail.com>
2022-09-02 18:05:05 +0500

    not jump on collision left and right

--- code/player.py
+++ code/player.py
@@ -45,13 +45,21 @@ class Player(pygame.sprite.Sprite):
         else:
             flipped_image = pygame.transform.flip(image, True, False)
             self.image = flipped_image
-        # set rectangle
-        if self.on_ground:
+            # set the rect
+        if self.on_ground and self.on_right:
+            self.rect = self.image.get_rect(bottomright=self.rect.bottomright)
+        elif self.on_ground and self.on_left:
+            self.rect = self.image.get_rect(bottomleft=self.rect.bottomleft)
+        elif self.on_ground:
             self.rect = self.image.get_rect(midbottom=self.rect.midbottom)
+
+        elif self.on_ceiling and self.on_right:
+            self.rect = self.image.get_rect(topright=self.rect.topright)
+        elif self.on_ceiling and self.on_left:
+            self.rect = self.image.get_rect(topleft=self.rect.topleft)
         elif self.on_ceiling:
             self.rect = self.image.get_rect(midtop=self.rect.midtop)
-        else:
-            self.rect = self.image.get_rect(center=self.rect.center)
+
 
 
 
