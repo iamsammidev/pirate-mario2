a82ba88c6fc994b9d39cf5331c441ea9c386eed0
Samir <sami.mir.aner@gmail.com>
2022-10-16 13:22:07 +0500

    setting up all static layers

--- code/level.py
+++ code/level.py
@@ -1,5 +1,5 @@
 import pygame
-from tiles import Tile, StaticTile
+from tiles import Tile, StaticTile, Crate
 from player import Player
 from particles import ParticleEffect
 from support import import_csv_layout, import_cut_graphics
@@ -16,6 +16,14 @@ class Level:
         terrain_layout = import_csv_layout(level_data['terrain'])
         self.terrain_sprites = self.create_tile_group(terrain_layout, 'terrain')
 
+        # grass setup
+        grass_layout = import_csv_layout(level_data['grass'])
+        self.grass_sprites = self.create_tile_group(grass_layout, 'grass')
+
+        # crates
+        crate_layout = import_csv_layout(level_data['crates'])
+        self.crate_sprites = self.create_tile_group(crate_layout, 'crates')
+
     def create_tile_group(self, layout, type):
         sprite_group = pygame.sprite.Group()
 
@@ -28,15 +36,34 @@ class Level:
                     if type == 'terrain':
                         terrain_tile_list = import_cut_graphics('../graphics/terrain/terrain_tiles.png')
                         tile_surface = terrain_tile_list[int(val)]
-                        sprite = StaticTile(tile_size, x, y,tile_surface)
-                        sprite_group.add(sprite)
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+
+                    if type == 'grass':
+                        grass_tile_list = import_cut_graphics('../graphics/decoration/grass/grass.png')
+                        tile_surface = grass_tile_list[int(val)]
+                        sprite = StaticTile(tile_size, x, y, tile_surface)
+
+                    if type == 'crates':
+                        sprite = Crate(tile_size, x, y)
+
+                    sprite_group.add(sprite)
 
         return sprite_group
 
     def run(self):
         # run the entire game / level
-        self.terrain_sprites.draw(self.display_surface)
+
+        # terrain
         self.terrain_sprites.update(self.world_shift)
+        self.terrain_sprites.draw(self.display_surface)
+
+        # grass
+        self.grass_sprites.update(self.world_shift)
+        self.grass_sprites.draw(self.display_surface)
+
+        # crate
+        self.crate_sprites.update(self.world_shift)
+        self.crate_sprites.draw(self.display_surface)
 # OLD VERSION
 class Level_old:
     def __init__(self, level_data, surface):
