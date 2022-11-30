9279960e6b92326a314c462029bb25e7b00ceca9
Samir <sami.mir.aner@gmail.com>
2022-11-30 21:17:46 +0500

    the overworld logic

--- code/overworld.py
+++ code/overworld.py
@@ -2,7 +2,7 @@ import pygame
 from game_data import levels
 
 class Node(pygame.sprite.Sprite):
-    def __init__(self, pos, status):
+    def __init__(self, pos, status, icon_speed):
         super().__init__()
         self.image = pygame.Surface((100, 80))
         if status == 'available':
@@ -11,12 +11,18 @@ class Node(pygame.sprite.Sprite):
             self.image.fill('grey')
         self.rect = self.image.get_rect(center=pos)
 
+        self.detection_zone = pygame.Rect(self.rect.centerx-(icon_speed/2),self.rect.centery-(icon_speed/2),icon_speed, icon_speed)
+
 class Icon(pygame.sprite.Sprite):
     def __init__(self, pos):
         super().__init__()
+        self.pos = pos
         self.image = pygame.Surface((20, 20))
         self.image.fill('blue')
         self.rect = self.image.get_rect(center=pos)
+
+    def update(self):
+        self.rect.center = self.pos
 
 class Overworld:
     def __init__(self, start_level, max_level, surface):
@@ -25,6 +31,11 @@ class Overworld:
         self.display_surface = surface
         self.max_level = max_level
         self.current_level = start_level
+
+        # movement logic
+        self.moving = False
+        self.move_direction = pygame.math.Vector2(0, 0)
+        self.speed = 8
 
         # sprites
         self.setup_nodes()
@@ -35,9 +46,9 @@ class Overworld:
 
         for index, node_data in enumerate(levels.values()):
             if index <= self.max_level:
-                node_sprites = Node(node_data['node_pos'], 'available')
+                node_sprites = Node(node_data['node_pos'], 'available', self.speed)
             else:
-                node_sprites = Node(node_data['node_pos'], 'locked')
+                node_sprites = Node(node_data['node_pos'], 'locked', self.speed)
             self.nodes.add(node_sprites)
 
     def draw_paths(self):
@@ -52,18 +63,38 @@ class Overworld:
     def input(self):
         keys = pygame.key.get_pressed()
 
-        if keys[pygame.K_RIGHT] and self.current_level < self.max_level:
-            self.move_direction = self.get_movement_data()
-            self.current_level += 1
-        elif keys[pygame.K_LEFT] and self.current_level > 0:
-            self.current_level -= 1
+        if not self.moving:
+            if keys[pygame.K_RIGHT] and self.current_level < self.max_level:
+                self.move_direction = self.get_movement_data('next')
+                self.current_level += 1
+                self.moving = True
+            elif keys[pygame.K_LEFT] and self.current_level > 0:
+                self.move_direction = self.get_movement_data('previous')
+                self.current_level -= 1
+                self.moving = True
+
+    def get_movement_data(self, target):
+        start = pygame.math.Vector2(self.nodes.sprites()[self.current_level].rect.center)
+
+        if target == 'next':
+            end = pygame.math.Vector2(self.nodes.sprites()[self.current_level + 1].rect.center)
+        else:
+            end = pygame.math.Vector2(self.nodes.sprites()[self.current_level - 1].rect.center)
+
+        return (end - start).normalize()
 
     def update_icon_pos(self):
-        pass
+        if self.moving and self.move_direction:
+            self.icon.sprite.pos += self.move_direction * self.speed
+            target_node = self.nodes.sprites()[self.current_level]
+            if target_node.detection_zone.collidepoint(self.icon.sprite.pos):
+                self.moving = False
+                self.move_direction = pygame.math.Vector2(0, 0)
 
     def run(self):
         self.input()
         self.update_icon_pos()
+        self.icon.update()
         self.draw_paths()
         self.nodes.draw(self.display_surface)
         self.icon.draw(self.display_surface)
