b1825c934869b72f4fa1cb1ef9a6888e9bbad276
Samir <sami.mir.aner@gmail.com>
2022-12-08 17:39:41 +0500

    Player collisions with the enemies

--- code/particles.py
+++ code/particles.py
@@ -5,11 +5,13 @@ class ParticleEffect(pygame.sprite.Sprite):
     def __init__(self, pos, type):
         super(). __init__()
         self.frame_index = 0
-        self.animation_speed = 0.5
+        self.animation_speed = 0.3
         if type == 'jump':
             self.frames = import_folder('../graphics/character/dust_particles/jump')
         if type == 'land':
             self.frames = import_folder('../graphics/character/dust_particles/land')
+        if type == 'explosion':
+            self.frames = import_folder('../graphics/enemy/explosion')
         self.image = self.frames[self.frame_index]
         self.rect = self.image.get_rect(center=pos)
 
